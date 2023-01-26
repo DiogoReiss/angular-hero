d6716e51a80d8e4f5cf239fd28ace535bd1f9bcf
Diogo Reis <ctw02705@itdesks-MacBook-Pro-5.local>
2023-01-26 17:16:53 +0000

    feat: button test cases and md test document

--- src/app/components/button/button.component.spec.ts
+++ src/app/components/button/button.component.spec.ts
@@ -1,6 +1,7 @@
 import 'zone.js';
 import 'zone.js/testing';
 
+import { Component } from '@angular/core';
 import { waitForAsync, ComponentFixture, TestBed } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
@@ -12,7 +13,6 @@ import { ButtonComponent } from './button.component';
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
-
   TestBed.initTestEnvironment(
     BrowserDynamicTestingModule,
     platformBrowserDynamicTesting()
@@ -25,10 +25,34 @@ describe('ButtonComponent', () => {
 
     fixture = TestBed.createComponent(ButtonComponent);
     component = fixture.componentInstance;
+    component.color = 'green';
+    component.text = 'test';
     fixture.detectChanges();
   }));
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should change the component color', () => {
+    expect(
+      fixture.nativeElement.querySelector('button').style.backgroundColor
+    ).toEqual('green');
+  });
+
+  it('should change the component text', () => {
+    expect(fixture.nativeElement.querySelector('button').textContent).toEqual(
+      'test'
+    );
+  });
+
+  it('should emit an event', () => {
+    component.btnClick.subscribe((res) => {
+      // we are only emitting an event here, so if this event are emitted
+      // we'll reach this point, and the test will pass
+      expect(true).toBeTruthy();
+    });
+
+    component.onClick();
   });
 });
